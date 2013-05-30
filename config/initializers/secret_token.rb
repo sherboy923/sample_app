476c18f129457dd53c89a07c761d8e157448eb97
root <root@FORTRESS.server.local>
2013-05-30 18:19:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1381fbf2ef0f892ec23df2acf9cbb37b37c873e51341591f62468f1cbaf4f0633ca3021ad4b84a1d93e21c036c6732843488b7e5fcf87904579d694a613f36db'
