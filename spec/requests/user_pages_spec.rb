2c2332d57e25b4e0c3b60be04ca073e162f9f924
Sherman St. Hill <scsthill@gmail.com>
2013-06-17 14:57:06 -0500

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,10 +54,19 @@ describe "User pages" do
 
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
+		let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+		let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
 		before { visit user_path(user) }
 
 		it { should have_selector('h1', 	text: user.name) }
 		it { should have_selector('title', 	text: user.name) }
+
+		describe "microposts" do
+			it { should have_content(m1.content) }
+			it { should have_content(m2.content) }
+			it { should have_content(user.microposts.count) }
+		end
 	end
 
 	describe "Signup page" do
@@ -87,23 +96,24 @@ describe "User pages" do
 
 		describe "with valid information" do
 			before do
-				fill_in "Name",			with: "Example User"
-				fill_in "Email",		with: "user@example.com"
-				fill_in "Password",		with: "foobar"
-				fill_in "Confirmation",	with: "foobar"
-			end
-
-			describe "after saving the user" do
-				before { click_button submit }
-				let(:user) { User.find_by_email('user@example.com') }
-
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-				it { should have_link('Sign out')}
+				fill_in "Name",				with: "Example User"
+				fill_in "Email",			with: "user@example.com"
+				fill_in "Password",			with: "foobar"
+				fill_in "Confirm Password",	with: "foobar"
 			end
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+
+				let(:user) { User.find_by_email('user@example.com') }
+
+				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_link('Sign out') }
 			end
 		end
 	end
